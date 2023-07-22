74662a0fe931000851b9f8e3abd103813dc6f54a
drisyaathul <drisyak809@gmail.com>
2023-07-22 22:03:15 +0800

    Logical Problems

---
+++ src/com/bridgelabz/FibonacciSeries.java
@@ -0,0 +1,12 @@
+package com.bridgelabz;
+/*
+Fibonacci series is a special type of series in which the next term is the sum of the
+previous two terms. For example, if 0 and 1 are the two previous terms in a series, then
+the next term will be 1(0+1).
+ */
+
+public class FibonacciSeries {
+    public static void main(String[] args) {
+        System.out.println("");
+    }
+}
